6531acf1d34982a30357c5feb035b1ee17b910dc
LunaAbreu0202 <165338430+LunaAbreu0202@users.noreply.github.com>
2024-04-11 19:43:06 -0300

    Add files via upload

---
+++ menor valor.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+  float num1, num2, num3;
+  
+  printf ("Digite tres valores: ");
+  scanf ("%f %f %f", &num1 , &num2, &num3);
+  
+  if (num1 <= num2 && num1 <= num3) {
+       printf ("%.2f é o menor valor.\n", num1);
+} else if (num2 <= num1 && num2 <= num3) {
+    printf ("%.2f é o menor valor.\n", num2);
+} else { 
+    printf ("%.2f é o menor valor.\n", num3);
+}
+    return 0;
+}
