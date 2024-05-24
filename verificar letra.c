1aec76b34be7193caa6de25131e6b8e985c66dde
LunaAbreu0202 <165338430+LunaAbreu0202@users.noreply.github.com>
2024-05-23 21:03:15 -0300

    Add files via upload

---
+++ verificar letra.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char palavra[20];
+   
+   printf ("Escreva uma palavra: ");
+   scanf ("%s",palavra);
+   
+  printf ("A palavra digitada foi: %s", palavra);
+  
+  if (palavra[0] == 'A' || 'a') {
+    printf("\n Palavra inicia com a letra A\n");
+}
+
+   
+    
+       
+  
+  
+   
+   
+
+    return 0;
+}
